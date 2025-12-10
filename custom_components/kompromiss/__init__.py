620b0c0de6e3a3cbe6b3b29c565bd66506bccad5
Jonathan Jogenfors <jonathan@jogenfors.se>
2025-12-10 11:30:08 +0100

    chore: add config flow in manifest

--- custom_components/kompromiss/__init__.py
+++ custom_components/kompromiss/__init__.py
@@ -1,25 +1,23 @@
-"""Kompromiss component"""
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from kompromiss import const
 
-PLATFORMS: list[str] = ["switch", "sensor"]
-
 
 async def async_setup() -> bool:
-    """Set up the Kompromiss component."""
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    hass.data.setdefault(const.DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(config_entry, const.PLATFORMS)
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        config_entry, const.PLATFORMS
+    )
     if unload_ok:
-        hass.data[const.DOMAIN].pop(entry.entry_id, None)
+        hass.data[const.DOMAIN].pop(config_entry.entry_id, None)
     return unload_ok
