575f8c326ffe9a24617413728371cd5b71b86b6c
Jonathan Jogenfors <jonathan@jogenfors.se>
2025-12-10 13:47:51 +0100

    chore: setup sensor data

--- custom_components/kompromiss/sensor.py
+++ custom_components/kompromiss/sensor.py
@@ -1,24 +1,15 @@
 """Sensor entity for the Kompromiss heat price optimizer."""
 
 from __future__ import annotations
-import logging
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from . import const
-
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(
-    hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities
+    _hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities
 ):
-    data = hass.data[const.DOMAIN][config_entry.entry_id]
-
-    _LOGGER.debug("async_setup_entry: %s", data)
-
     async_add_entities([SimulatedOutdoorTemperatureSensor(config_entry)])
 
 
